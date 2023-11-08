2e4bc2d19b248605705d457f3c037443bbef9ab9
KRLY95 <levinekelsi@gmail.com>
2023-11-07 20:43:30 -0500

    Added the Slot class header + cpp files.

--- VendingMachineV2/VendingMachineV2/MainForm.cpp
+++ VendingMachineV2/VendingMachineV2/MainForm.cpp
@@ -2,6 +2,14 @@
 
 using namespace System;
 using namespace System::Windows::Forms;
+using namespace VendingMachineV2;
+
+void MainForm::IncrementButton_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	counterCount = 0;
+	counter->Text = counterCount.ToString();
+}
+
 void main(array<String^>^ args)
 {
 	Application::EnableVisualStyles();
